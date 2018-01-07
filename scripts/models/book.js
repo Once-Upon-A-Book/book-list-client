c6f809b7b9f0e8ce03fec52750b865d0e97103e9
amgranad <ambergkim@gmail.com>
2018-01-06 16:10:52 -0800

    server and model requirements

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,12 +1,20 @@
 'use strict';
 
 //const _API_URL_ = 'https://al-ak-ec-booklist.herokuapp.com';
-const _API_URL_ = 'http://localhost:3000'
-$.getJSON(`${_API_URL_}/test`)
-    .then(res => {
-        $('#data-spot').append(JSON.stringify(res))
-  }
-        res.forEach(thing => {
-            $('#data-spot').append(`<li>${books.title}</li>`)
-        })
-    }).catch(error => console.error(error))
+const _API_URL_ = 'http://localhost:3000';
+
+// $.getJSON(`${_API_URL_}/test`)
+//     .then(res => {
+//         $('#data-spot').append(JSON.stringify(res))
+//   }
+//         res.forEach(thing => {
+//             $('#data-spot').append(`<li>${books.title}</li>`)
+//         })
+//     }).catch(error => console.error(error))
+
+$.get(`${_API_URL_}/api/v1/books`)
+  .then(res => {
+    res.forEach(data => {
+      console.log(data);
+    });
+  }).catch(error => console.error(error));
