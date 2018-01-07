662b4be7257982e683cc77763c1f0548156bd4dd
amgranad <ambergkim@gmail.com>
2018-01-06 17:15:39 -0800

    mvp

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -11,10 +11,26 @@ const _API_URL_ = 'http://localhost:3000';
 //             $('#data-spot').append(`<li>${books.title}</li>`)
 //         })
 //     }).catch(error => console.error(error))
+var app = app || {};
 
-$.get(`${_API_URL_}/api/v1/books`)
-  .then(res => {
-    res.forEach(data => {
-      console.log(data);
-    });
-  }).catch(error => console.error(error));
+(function(module) {
+  function Book(rawBookObj) {
+
+  }
+
+  Book.all = [];
+
+  Book.getBooks = function(callback) {
+    console.log('got to getBooks method');
+    $.get(`${_API_URL_}/api/v1/books`)
+      .then(res => {
+        res.forEach(data => {
+          Book.all.push(data);
+        });
+        console.log('get books method array: ' + Book.all);
+        callback();
+      }).catch(error => console.error(error));
+  };
+
+  module.Book = Book;
+})(app);
