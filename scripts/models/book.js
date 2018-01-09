5e068658df8393615f2e89e2aaf05f4185a453f7
amgranad <ambergkim@gmail.com>
2018-01-08 22:09:12 -0800

    cleanup

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -3,14 +3,6 @@
 //const _API_URL_ = 'https://al-ak-ec-booklist.herokuapp.com';
 const _API_URL_ = 'http://localhost:3000';
 
-// $.getJSON(`${_API_URL_}/test`)
-//     .then(res => {
-//         $('#data-spot').append(JSON.stringify(res))
-//   }
-//         res.forEach(thing => {
-//             $('#data-spot').append(`<li>${books.title}</li>`)
-//         })
-//     }).catch(error => console.error(error))
 var app = app || {};
 
 (function(module) {
