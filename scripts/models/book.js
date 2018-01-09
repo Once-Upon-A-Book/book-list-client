b68a81a2da834fcfd80cc4ba350499ad7dc23c2b
alicialycan <alicialycan@gmail.com>
2018-01-08 20:34:01 -0800

    updates

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //const _API_URL_ = 'https://al-ak-ec-booklist.herokuapp.com';
-const _API_URL_ = 'http://localhost:3000';
+const _API_URL_ = 'http://localhost:3000/api/v1/books';
 
 // $.getJSON(`${_API_URL_}/test`)
 //     .then(res => {
@@ -21,13 +21,11 @@ var app = app || {};
   Book.all = [];
 
   Book.getBooks = function(callback) {
-    console.log('got to getBooks method');
-    $.get(`${_API_URL_}/api/v1/books`)
+    $.getJSON(`${_API_URL_}/api/v1/books`)
       .then(res => {
         res.forEach(data => {
           Book.all.push(data);
         });
-        console.log('get books method array: ' + Book.all);
         callback();
       }).catch(error => console.error(error));
   };
