09a28e59a85c2290a6c3c354e79529b227bad98b
alicialycan <alicialycan@gmail.com>
2018-01-09 21:32:21 -0800

    add book views

---
+++ scripts/views/book-details-page.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+(module => {
+
+  const bookDetailsPage = {};
+
+  booksDetailPage.init = (book) => {
+    $('#book-title').text(book.title);
+    $('#book-author').text(book.author);
+
+    //$('book-detail-page').show()
+  };
+
+  module.bookDetailsPage = bookDetailsPage
+})(app)
---
+++ scripts/views/book-list-page.js
@@ -0,0 +1,23 @@
+var app = app || {};
+
+(module => {
+
+  const bookListPage = {};
+
+  bookListPage.initIndexView = (books) => {
+   
+    books.forEach(book => {
+      $('#book-list').append(`<li data-id="${book.book_id}">${book.title}"</li>`);
+    });
+
+    $('#book-list').on('click', 'li', (event) => { //event handler to capture the click
+      $(event.target).data('id');
+      page('/books/' + id);
+    });
+
+    $('#book-list-page').show();
+  };
+
+  module.bookListPage = bookListPage
+
+})(app);
